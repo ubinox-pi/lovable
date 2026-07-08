4817dee200b617c70a97f8d96585188a617ae1a6
Ramjee prasad <ashish23481@gmail.com>
2026-07-08 16:01:42 +0530

    Initial commit

---
+++ src/main/java/com/lovable/LovableApplication.java
@@ -0,0 +1,13 @@
+package com.lovable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovableApplication.class, args);
+    }
+
+}
